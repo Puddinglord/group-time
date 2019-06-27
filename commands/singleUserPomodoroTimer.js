8c0fb30fb745a4795576e50385a7f08d9f16b69f
Dakotah Cote <puddinglord3@gmail.com>
2019-06-27 10:39:56 -0400

    made a pomodoro session way cleaner

--- commands/singleUserPomodoroTimer.js
+++ commands/singleUserPomodoroTimer.js
@@ -1,42 +1,37 @@
 module.exports = {
 	name: 'pomodoro',
 	description: 'A stadndard pomodoro timer for a single user. Uses the standard 25 minute work and 5 minute break timers. Four work sessions gets you a 15 minute break.',
+	workDuration: 1500000,
+	shortBreakDuration: 300000,
+	longBreakDuration: 900000,
+	workMessage: 'Work session complete! Time for a well deserved break. I\'ll let you know when the break is over so we can start working again.',
+	shortBreakMessage: 'Short break is over! Let\'s get back to work :white_check_mark:',
+	longBreakMessage: '',
+	sessionCount: 1,
 	execute(message, args) {
-		const workDuration = 1500000;
-		const shortBreakDuration = 300000;
-		const longBreakDuration = 900000;
-
-		const workMessage = 'Work session complete! Time for a well deserved break. I\'ll let you know when the break is over so we can start working again.';
-		const shortBreakMessage = 'Short break is over! Let\'s get back to work :white_check_mark:';
-		const longBreakMessage = '';
-
 		message.reply('Starting a standard pomodoro session. We will work for 25 minutes and then take a 5 minute break.');
 		message.reply('The session has officially started! :fire:');
 
-		setTimeout(() => {
-			message.reply(workMessage);
+		this.pomomodoSession(message);
+	},
+	pomomodoSession(message) {
+		if(this.sessionCount < 4) {
 			setTimeout(() => {
-				message.reply(shortBreakMessage);
+				message.reply(this.workMessage);
 				setTimeout(() => {
-					message.reply(workMessage);
-					setTimeout(() => {
-						message.reply(shortBreakMessage);
-						setTimeout(() => {
-							message.reply(workMessage);
-							setTimeout(() => {
-								message.reply(shortBreakMessage);
-								setTimeout(() => {
-									message.reply(workMessage);
-									setTimeout(() => {
-										message.reply(longBreakMessage);
-										message.reply('Congraduations! You\'ve compeleted a pomodoro session :confetti_ball: If you want to start another just let me know.');
-									}, longBreakDuration);
-								}, workDuration);
-							}, shortBreakDuration);
-						}, workDuration);
-					}, shortBreakDuration);
-				}, workDuration);
-			}, shortBreakDuration);
-		}, workDuration);
+					message.reply(this.shortBreakMessage);
+					this.sessionCount++;
+					this.pomomodoSession(message);
+				}, this.shortBreakDuration);
+			}, this.workDuration);
+		}
+		else {
+			setTimeout(() => {
+				message.reply(this.workMessage);
+				setTimeout(() => {
+					message.reply(this.longBreakMessage);
+				}, this.longBreakDuration);
+			}, this.workDuration);
+		}
 	},
 };
